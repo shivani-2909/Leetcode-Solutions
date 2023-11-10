77409667052422a1559bf0b0f85679968922bee7
Shivani Arora <98224041+shivani-2909@users.noreply.github.com>
2023-11-10 17:24:07 +0530

    Time: 72 ms (89.75%), Space: 96.9 MB (54.51%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
@@ -1,34 +1,31 @@
+
 class Solution {
-    public int[] restoreArray(int[][] vals) {
-        Map<Integer, int[]> pairs = new HashMap<>();
-        for (int i = 0; i < vals.length; i++) {
-            if (pairs.containsKey(vals[i][0])) {
-                pairs.get(vals[i][0])[1] = vals[i][1];
-            } else {
-                pairs.put(vals[i][0], new int[] {vals[i][1], -1000000});
-            }
-            if (pairs.containsKey(vals[i][1])) {
-                pairs.get(vals[i][1])[1] = vals[i][0];
-            } else {
-                pairs.put(vals[i][1], new int[] {vals[i][0], -1000000});
-            }
+    public int[] restoreArray(int[][] adjacentPairs) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] pair : adjacentPairs) {
+            graph.computeIfAbsent(pair[0], k -> new ArrayList<>()).add(pair[1]);
+            graph.computeIfAbsent(pair[1], k -> new ArrayList<>()).add(pair[0]);
         }
-        int[] result = new int[vals.length + 1];
-        int start = -1000000;
-        for (Map.Entry<Integer, int[]> entry : pairs.entrySet()) {
-            if (entry.getValue()[1] == -1000000) {
-                start = entry.getKey();
+
+        List<Integer> result = new ArrayList<>();
+
+        for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
+            if (entry.getValue().size() == 1) {
+                result.add(entry.getKey());
+                result.add(entry.getValue().get(0));
+                break;
             }
         }
-        result[0] = start;
-        int left = -1000000;
-        for (int i = 1; i < result.length; i++) {
-            int[] val = pairs.get(start);
-            int newval = (val[0] == left ? val[1] : val[0]);
-            result[i] = newval;
-            left = start;
-            start = newval;
+
+        while (result.size() < adjacentPairs.length + 1) {
+            int last = result.get(result.size() - 1);
+            int prev = result.get(result.size() - 2);
+            List<Integer> candidates = graph.get(last);
+            int nextElement = candidates.get(0) != prev ? candidates.get(0) : candidates.get(1);
+            result.add(nextElement);
         }
-        return result;
+
+        return result.stream().mapToInt(Integer::intValue).toArray();        
     }
 }
