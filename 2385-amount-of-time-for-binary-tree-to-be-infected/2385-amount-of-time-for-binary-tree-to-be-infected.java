9e421e1e4a7e1dec18cb3949e233fd17639ea8c4
Shivani Arora <98224041+shivani-2909@users.noreply.github.com>
2024-01-10 15:37:14 +0530

    Time: 167 ms (16.44%), Space: 132.8 MB (6.87%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -14,51 +14,44 @@
  * }
  */
 class Solution {
-    public int amountOfTime(TreeNode root, int start) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        convert(root, 0, map);
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        int minute = 0;
-        Set<Integer> visited = new HashSet<>();
-        visited.add(start);
+     Map<Integer, Set<Integer>> graph = new HashMap<>();
 
-        while (!q.isEmpty()) {
-            int levelSize = q.size();
-            while (levelSize > 0) {
-                int current = q.poll();
+  private void convert(TreeNode node, int parent) {
+    graph.put(node.val, new HashSet<>());
 
-                for (int num : map.get(current)) {
-                    if (!visited.contains(num)) {
-                        visited.add(num);
-                        q.add(num);
-                    }
-                }
-                levelSize--;
-            }
-            minute++;
-        }
-        return minute - 1;
+    if (parent > 0)
+      graph.get(node.val).add(parent);
+
+    if (node.left != null) {
+      graph.get(node.val).add(node.left.val);
+      convert(node.left, node.val);
     }
-
-    void convert(TreeNode current, int parent, Map<Integer, Set<Integer>> map) {
-        if (current == null) {
-            return;
-        } 
-        if (!map.containsKey(current.val)) {
-            map.put(current.val, new HashSet<>());
-        }
-        Set<Integer> adjacentList = map.get(current.val);
-        if (parent != 0) {
-            adjacentList.add(parent);
-        } 
-        if (current.left != null) {
-            adjacentList.add(current.left.val);
-        } 
-        if (current.right != null) {
-            adjacentList.add(current.right.val);
-        }
-        convert(current.left, current.val, map);
-        convert(current.right, current.val, map);
+    if (node.right != null) {
+      graph.get(node.val).add(node.right.val);
+      convert(node.right, node.val);
     }
+  }
+
+  public int amountOfTime(TreeNode root, int start) {
+    convert(root, 0);
+
+    var queue = new ArrayDeque<Integer>();
+    queue.offer(start);
+
+    var seen = new HashSet<>();
+    var min = -1;
+    
+    while (!queue.isEmpty()) {
+      for (var i = queue.size(); i > 0; i--) {
+        var node = queue.poll();
+        seen.add(node);
+
+        for (var neighbor : graph.get(node))
+          if (!seen.contains(neighbor))
+            queue.offer(neighbor);
+      }
+      min++;
+    }
+    return min;
+  }
 }
