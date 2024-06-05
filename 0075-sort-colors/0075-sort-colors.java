5fba8084ca0e9c2dfb926f2de532a8f172f085db
Shivani Arora <98224041+shivani-2909@users.noreply.github.com>
2024-06-06 01:13:00 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (88.87%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,19 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
-        if (nums.length <= 1) {
-            return;  // If the array has 1 or 0 elements, it's already sorted
-        }
-
-        int s = 0;
-        int e = nums.length - 1;
+       int low = 0;
+        int mid=0;
+        int high = nums.length-1;
         
-        // We need to loop through the array and use a modified bubble sort approach
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = 0; j < nums.length - 1 - i; j++) {
-                if (nums[j] > nums[j + 1]) {
-                    swap(j, j + 1, nums);
-                }
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(low,mid,nums);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else if(nums[mid]==2){
+                swap(mid,high,nums);
+                high--;
             }
+            
         }
     }
 
