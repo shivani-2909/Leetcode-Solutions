44699b2fe73a444daba9fcefddbe27eed2cddc47
Shivani Arora <98224041+shivani-2909@users.noreply.github.com>
2024-07-15 20:54:12 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (6.73%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        return check(n);
+    }
+    public static boolean check(int n){
+    if (n == 1) {
+            return true;
+        }
+        if (n % 2 != 0) {
+            return false; 
+        }
+        return check(n / 2); 
+    }
+}
